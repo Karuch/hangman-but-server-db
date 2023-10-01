28d4bf42b028b87ca27aa27e4d1b683caee15b8d
Karuch <talk159820@gmail.com>
2023-10-01 19:49:48 +0300

    now support input check

--- start.go
+++ start.go
@@ -36,11 +36,23 @@ func main() {
 	fmt.Println(letters_list_blank)
 
 	var guess string
+	var possibilities_list = [26]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 	fmt.Scan(&guess)
 	if strings.EqualFold(guess, word) {
 		fmt.Println("Correct!")
 	}
-	
+	for index, _ := range possibilities_list {
+		if  len(guess) > 1 {
+			fmt.Printf("`%v` is not a valid character (contains more then 1).", guess)
+			break
+		} else if strings.EqualFold(guess, possibilities_list[index]) {
+			fmt.Println("yes")
+			break
+		} else {
+			fmt.Printf("`%v` is not a valid character. (english letters only)", guess)
+			break
+		}
+	}
 	
 
 
