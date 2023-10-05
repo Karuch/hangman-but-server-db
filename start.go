44b3af123f060c7ffbb79516ee662aa74587370c
Karuch <talk159820@gmail.com>
2023-10-05 13:57:47 +0300

    I think buffer working know, however need to clean the mess

--- start.go
+++ start.go
@@ -1,53 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"strings"
 	"bufio"
+	"fmt"
 	"os"
+	"strings"
+	"reflect"
 )
 
-func scan_input(prompt string) string { //need this to support spaces in go input
-	fmt.Print(prompt)
-	scanner := bufio.NewScanner(os.Stdin)
-	if scanner.Scan() {
-		return scanner.Text()
-	}
-	if err := scanner.Err(); err != nil {
-		return "Error reading input: " + err.Error()
-	}
-	return "" 
-}
 
-func input_checker(word string) (string, bool) {
+func input_checker(word string, scanner *bufio.Scanner) (string, bool) {
 	var guess string
 	var possibilities_list = [27]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 	var guessed_true bool = false
-
 	guess_loop:
 	for 0 < 1 {
-		fmt.Scan(&guess)
+		scanner.Scan()
+		guess := scanner.Text()
 		fmt.Println(guess, "<-")
-		if strings.EqualFold(guess, word) {
-			guessed_true = true
-			break
+		if len(guess) > 1 {
+			if !strings.EqualFold(guess, word) {
+				//fmt.Printf("`%v` is incorrect word, try again\n", guess) ***************
+				continue 
+			} else {
+				guessed_true = true
+				break guess_loop
+			}
 		}
+
 		guess = strings.ToUpper(guess)
 		var possible_letter bool = false
 		for index, _ := range possibilities_list {
-			if len(guess) > 1 {
-				fmt.Printf("`%v` is not a valid character. (contains more then 1)\n", guess)
-				break
-			} else if guess == possibilities_list[index] {
+			if guess == possibilities_list[index] {
 				fmt.Println("yes")
 				possible_letter = true
 				break guess_loop
 			} else {
 				continue
-			}
+			}										
 		}
+
 		if !possible_letter {
-			fmt.Printf("`%v` is not a valid character. (english letters only)\n", guess)
+			fmt.Println("`%v` is not a valid character. (english letters only)", guess)
 		}
 	}
 	return guess, guessed_true
@@ -55,17 +49,19 @@ func input_checker(word string) (string, bool) {
 
 
 func main() {
-	var choice string
+	scanner := bufio.NewScanner(os.Stdin)
 	for 0 < 1 {
 		fmt.Println("--Welcome to hangman (CLI Version)--\nto start type `start`\nto quit type `quit`")
-		fmt.Scan(&choice)
+		scanner.Scan()
+		fmt.Println("Type of x:", reflect.TypeOf(scanner))
+		choice := scanner.Text()
 		if strings.EqualFold(choice, "start") {
 			break
 		} else if strings.EqualFold(choice, "quit") {
 			fmt.Println("exit...")
 			return 
 		} else {
-			fmt.Printf("`%v` is wrong option, only `start` or `quit` is available", choice)
+			fmt.Printf("`%v` is wrong option, only `start` or `quit` is available\n", choice)
 		}
 	}
 
@@ -75,7 +71,7 @@ func main() {
 	//checking if guess input is allowed
 	var guess string
 	var guessed_true bool
-	guess, guessed_true = input_checker(word)
+	guess, guessed_true = input_checker(word, scanner)
 	if guessed_true {
 		fmt.Println("TRUE WORD!!!")
 	}
